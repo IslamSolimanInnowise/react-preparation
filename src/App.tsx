79e006cd35b1da8ec4c0972b94bd81c211a257f2
IslamSolimanInnowise <islam.soliman@innowise.com>
2025-01-16 17:38:18 +0300

    add useState component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import UseStateHook from "./components/hooks/state/UseStateHook/UseStateHook";
+
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>React Hooks</h1>
+      <UseStateHook />
+    </div>
+  );
 };
 export default App;
---
+++ src/components/hooks/state/UseStateHook/UseStateHook.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function iWillWorkEveryTime() {
+  console.log("See ?");
+  return 20;
+}
+
+const UseStateHook: React.FC = () => {
+  const [count, setCount] = useState(0);
+  const [myFuncVal, setMyFuncVal] = useState(iWillWorkEveryTime());
+
+  function handleClick() {
+    //[1] basic example
+    // setCount(count + 1);
+    setCount((prevCount) => prevCount + 1);
+
+    // [2] function state example
+    setMyFuncVal(myFuncVal + 1);
+    console.log(myFuncVal);
+  }
+
+  return (
+    <div>
+      <h2>UseState Hook</h2>
+      <button onClick={handleClick}>Current count is {count}</button>
+    </div>
+  );
+};
+export default UseStateHook;
